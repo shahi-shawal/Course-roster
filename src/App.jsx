3886aa107a7deb230dfa758f8a4a5751f9c1ec7d
Shawal <shahishawal@gmail.com>
2023-09-16 13:27:52 +0600

    update toast in remaing crediti and readme filet

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,38 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [cardSelect, setCard]= useState([])
+  const [totalCredit, setCredit]= useState(0);
+  const [remaning, setRemaining]= useState(0)
   const handelSelect =(select)=>{
        const findItem= cardSelect.find((carded)=> carded.id ===select.id)
-        if (findItem) {
+        let credit = select.credit
+       if (findItem) {
          return  toast.info("add only once")
         }
+        else{
+          cardSelect.forEach((item)=>{
+            credit = credit+item.credit
+          })
+          const totalRemaining = 20- credit;
+          if (credit > 20) {
+            return toast.warning("You Have no More Credit to Take it")
+          }else{
+            setCredit(credit)
+          setRemaining(totalRemaining);
+          }
+          
+        }
         const newSelect = [...cardSelect,select];
-        setCard(newSelect)
+        setCard(newSelect);
   }
+  
   return (
     <>
       <Header></Header>
       <div className='flex gap-1'>
         <ToastContainer />
       <Datas handelSelect={handelSelect}></Datas>
-      <Cart cardSelect={cardSelect}></Cart>
+      <Cart cardSelect={cardSelect} totalCredit={totalCredit} remaning={remaning}></Cart>
       </div>
     </>
   )
